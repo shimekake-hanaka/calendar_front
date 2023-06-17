166364b3de82742fdb07821d1f2682e72dffb2c2
shimekake-hanaka <shimekake.hanaka@icloud.com>
2023-06-17 18:16:59 +0900

    fullcalender

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import logo from './logo.svg';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import jaLocale from '@fullcalendar/core/locales/ja'; // 追加
 import './App.css';
+
 
 const name = '太郎';
 const age = '10';
 
 function App() {
-
-
   return (
     <div className="App">
       <h1>
-        holloworld
+        hollo world
       </h1>
       <p class="name">僕の名前は{name}です。{age}歳です。</p>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        locales={[jaLocale]}         // 追加
+        locale='ja'                  // 追加
+      />
     </div>
   );
 }
-
 export default App;

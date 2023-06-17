2372b2e89a34d3591fe9198ff0e127ff799f310a
shimekake-hanaka <shimekake.hanaka@icloud.com>
2023-06-17 17:33:51 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = '太郎';
+const age = '10';
+
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        holloworld
+      </h1>
+      <p>僕の名前は{name}です。{age}歳です。</p>
     </div>
   );
 }
